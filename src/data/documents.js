e22435139aaa0013c060c171847dfa78fe893154
lucatwzgroup <luca.cassier@twzgroup.be>
2025-04-07 10:08:26 +0200

    Update documents.js

--- src/data/documents.js
+++ src/data/documents.js
@@ -7,33 +7,33 @@ export const documents = [
     },
     {
       category: 'Odoo',
-      title: 'Odoo Inventory Management Manual',
-      description: 'Learn how to efficiently manage inventory, track stock movements, and generate reports in Odoo.',
-      link: '#'
+      title: 'Invoice mailbox verwerken',
+      description: 'Deze handleiding beschrijft het proces voor het verwerken van facturen uit de "Invoices (TWZ)" mailbox en het uploaden naar Odoo',
+      link: 'https://docs.google.com/document/d/1gq-EJQWz3cX90ITPeLjYU7R1Jz12o9334YWpycG0aik/edit?tab=t.0#heading=h.3l4rih2pezzz'
     },
     {
       category: 'Algemene',
-      title: 'Company Onboarding Process',
-      description: 'Step-by-step guide for new employee onboarding, including IT setup, account creation, and training resources.',
-      link: '#'
+      title: 'RS-6000 opnieuw toevoegen',
+      description: 'Deze handleiding beschrijft hoe je de RS-6000 opnieuw moet toevoegen.',
+      link: 'https://docs.google.com/document/d/1O_EFsicS1PvEBwov3xtcK3iUCrKkTgSWkTXjCUJ57J4/edit?usp=sharing'
+    },
+    {
+      category: 'Odoo',
+      title: 'Bill Approval',
+      description: 'Deze handleiding beschrijft de stappen voor het goedkeuren van een factuur in Odoo via de "Manager approval" functie.',
+      link: 'https://docs.google.com/document/d/13M5v2GaWATxcySZOnGUq971rnIaKn1lnEI1GD6aiz9k/edit?usp=sharing'
     },
     {
       category: 'Sirius',
-      title: 'Sirius Dashboard Configuration',
-      description: 'Instructions for customizing dashboards, creating widgets, and setting up personalized views.',
-      link: '#'
+      title: 'Hoe werkt Sirius?',
+      description: 'Deze handleiding beschrijft op een simpele manier de werking van Sirius.',
+      link: 'https://docs.google.com/document/d/109sFNj_aH9ep4yjNxbGv0lnrVM7nrYo2_U3VbDmJKjQ/edit?usp=sharing'
     },
     {
-      category: 'Sirius',
-      title: 'Sirius API Integration Guide',
-      description: 'Technical documentation for developers on how to integrate with Sirius APIs and extend functionality.',
-      link: '#'
-    },
-    {
-      category: 'Sirius',
-      title: 'Sirius Troubleshooting Guide',
-      description: 'Common issues and their solutions, error code references, and support escalation procedures.',
-      link: '#'
+      category: 'Algemene',
+      title: 'Lijst met telefoonnummers',
+      description: 'Dit document bevat een up-to-date lijst met al onze telefoonnummers.',
+      link: 'https://docs.google.com/spreadsheets/d/1e3BXLiZdc5C3bpqzDyypNrebLjuvExfS7u-97Mxc8aE/edit?gid=0#gid=0'
     },
     {
       category: 'Odoo',
